e14953b9c72f130117e1b1a6fd756a4fe7db6c4f
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-11-01 20:00:07 +0600

    add socket io

--- app.js
+++ app.js
@@ -1,49 +1,67 @@
 // external imports
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const loginRouter = require('./router/loginRouter')
-const usersRouter = require('./router/usersRouter')
-const inboxRouter = require('./router/inboxRouter')
+const express = require("express");
+const http = require("http");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const moment = require("moment");
 
 // internal imports
-const {notFoundHandler, errorHandler} = require('./middlewares/common/errorHandler')
+const loginRouter = require("./router/loginRouter");
+const usersRouter = require("./router/usersRouter");
+const inboxRouter = require("./router/inboxRouter");
+
+// internal imports
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandler");
 
 const app = express();
-
+const server = http.createServer(app);
 dotenv.config();
+
+// socket creation
+const io = require("socket.io")(server);
+global.io = io;
+
+// set comment as app locals
+app.locals.moment = moment;
 
 // database con
 mongoose.connect(process.env.MONGO_CONNECTION_STRING)
 .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err))
 
-// request parser
+// request parsers
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 // set view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // parse cookies
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // routing setup
-app.use('/', loginRouter);
-app.use('/users', usersRouter);
-app.use('/inbox', inboxRouter);
+app.use("/", loginRouter);
+app.use("/users", usersRouter);
+app.use("/inbox", inboxRouter);
 
-// error handling
-app.use(notFoundHandler)
+// 404 not found handler
+app.use(notFoundHandler);
 
 // common error handler
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(process.env.PORT, () => {
-    console.log(`app listening to port ${process.env.PORT}`)
-})
+server.listen(process.env.PORT, () => {
+  console.log(`app listening to port ${process.env.PORT}`);
+});
+
+
+
+
